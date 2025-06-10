f917dd7f30fa67fd0dbeef9cdea92aa91b9f66cc
shehata <ahmed.fcis2016@gmail.com>
2025-06-11 01:03:09 +0300

    - Edit CrossAxisAlignment of the ExperienceTile to be start

--- lib/presentation/widgets/sections/experience_section.dart
+++ lib/presentation/widgets/sections/experience_section.dart
@@ -10,10 +10,11 @@ class ExperienceSection extends StatelessWidget {
     return const SectionWrapper(
       title: 'Where I\'ve Worked',
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ExperienceTile(
             title: 'Flutter Developer',
-            company: 'Etisalat Egypt',
+            company: 'E& Egypt',
             period: 'Jun 2023 - Present',
             description:
                 'Worked on E& Cash modularization using Melos; enhanced code maintainability and integrated native modules; collaborated with design and QA teams for feature delivery.',
